335a05d72af8b0a15bdb2d23dbf799390439edf4
hulianghai <hulianghai@xmzhen.com>
2017-11-14 14:45:04 +0800

    celery` * 简单描述一下干了什么 What:
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- fund/celerys.py
+++ fund/celerys.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 app = Celery("FundPj")
--- fund/spider/tasks.py
+++ fund/spider/tasks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from celery import app
+from celerys import app
 import time
 
 
